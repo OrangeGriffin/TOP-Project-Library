26090855ecc04072bdbbebb66126fb8c6503245a
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-23 14:37:01 -0500

    Function to display each key/value of book object

---
+++ app.js
@@ -0,0 +1,59 @@
+let myLibrary = [];
+
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read}.`;
+  };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const squeezeMe = new Book("Squeeze Me", "Carl Hiaasen", 353, true);
+
+const bookCards = document.querySelector(".books-cards"); // Select the books-cards div
+
+const bookCard = document.createElement("div");
+bookCard.classList.add("book-card");
+
+// Create elements based on key/value pairs in theHobbit Ojbect, while
+// ignoring any functions within the Object
+const buildCard = function (bookOjbect) {
+  Object.entries(bookOjbect).forEach(([key, value]) => {
+    if (typeof value === "function") {
+      console.log(`${key} is a function`);
+    } else {
+      const newElement = document.createElement("p");
+      newElement.classList.add(`${key}`);
+      switch (`${key}`) {
+        case "title":
+          newElement.innerText = `Title: ${value}`;
+          break;
+        case "author":
+          newElement.innerText = `Author: ${value}`;
+          break;
+        case "pages":
+          newElement.innerText = `Pages: ${value}`;
+          break;
+        case "read":
+          if (value === false) {
+            newElement.innerText = "Not Read";
+            break;
+          } else {
+            newElement.innerText = "Read";
+            break;
+          }
+        default:
+          newElement.innerText = `${value}`;
+      }
+      bookCard.append(newElement);
+    }
+    bookCards.appendChild(bookCard);
+  });
+};
+
+buildCard(theHobbit);
+buildCard(squeezeMe);

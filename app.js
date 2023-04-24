45b05a27aee28a5403d251d9b6c9443dbe47c06b
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-04-24 08:41:12 -0500

    Add book to array. Invoke buildCards
    Bug: Currently the page displays duplicate books once
    a new book is submitted.

--- app.js
+++ app.js
@@ -25,44 +25,45 @@ const bookCards = document.querySelector(".books-cards"); // Select the books-ca
 
 // Create elements based on key/value pairs in theHobbit Ojbect, while
 // ignoring any functions within the Object
-const buildCard = myLibrary.forEach((bookOjbect) => {
-  const bookCard = document.createElement("div");
-  bookCard.classList.add("book-card");
-  Object.entries(bookOjbect).forEach(([key, value]) => {
-    if (typeof value === "function") {
-      /* Do Nothing */
-    } else {
-      const newElement = document.createElement("p");
-      newElement.classList.add(`${key}`);
-      switch (`${key}`) {
-        case "title":
-          newElement.innerText = `Title: ${value}`;
-          break;
-        case "author":
-          newElement.innerText = `Author: ${value}`;
-          break;
-        case "pages":
-          newElement.innerText = `Pages: ${value}`;
-          break;
-        case "read":
-          if (value === false) {
-            newElement.innerText = "Not Read";
+const buildCards = () => {
+  myLibrary.forEach((bookOjbect) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    Object.entries(bookOjbect).forEach(([key, value]) => {
+      if (typeof value === "function") {
+        /* Do Nothing */
+      } else {
+        const newElement = document.createElement("p");
+        newElement.classList.add(`${key}`);
+        switch (`${key}`) {
+          case "title":
+            newElement.innerText = `Title: ${value}`;
             break;
-          } else {
-            newElement.innerText = "Read";
+          case "author":
+            newElement.innerText = `Author: ${value}`;
             break;
-          }
-        default:
-          newElement.innerText = `${value}`;
+          case "pages":
+            newElement.innerText = `Pages: ${value}`;
+            break;
+          case "read":
+            if (value === false) {
+              newElement.innerText = "Not Read";
+              break;
+            } else {
+              newElement.innerText = "Read";
+              break;
+            }
+          default:
+            newElement.innerText = `${value}`;
+        }
+        bookCard.append(newElement);
       }
-      bookCard.append(newElement);
-    }
-    bookCards.appendChild(bookCard);
+      bookCards.appendChild(bookCard);
+    });
   });
-});
+};
 
-// buildCard(theHobbit);
-// buildCard(squeezeMe);
+buildCards();
 
 // Display a form for add book
 const displayAddBookForm = () => {
@@ -157,6 +158,9 @@ const submitForm = () => {
   // Place in myLibrary array
   myLibrary.push(newBook);
 
+  debugger;
+  buildCards();
+
   clearForm();
 };
 
@@ -184,7 +188,3 @@ submitBookBtn.addEventListener("click", () => {
   submitForm();
   showSnackbar();
 });
-
-// Working on myLibrary array
-// Place existing books in array
-// myLibrary.forEach((book) => buildCard(book));
